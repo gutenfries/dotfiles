3f8a0c4b149a6f517e2ebb68b800ad70dab70107
Marc Gutenberger <gutenfries@gmail.com>
2023-05-01 14:24:45 -0500

    make profile cleaner

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,21 @@
 #! /usr/bin/env pwsh
 
+# External Imports:
 Import-Module -Name PSReadLine
 Import-Module -Name CompletionPredictor
 
-# (neo)vim things...
+# Env:
 $env:PYTHONIOENCODING = "utf-8"
 $env:PYTHON_HOST_PROG = "C:\Python311\"
 $env:PYTHON3_HOST_PROG = "C:\Python311\"
+$env:lsd = ""
 
+# User Defined Functions:
 function Invoke-Admin () {
 	if ($Args.Count -eq 0) {
 		return "Invoke-Admin: No command specified"
 	}
-	else {
+ else {
 		# group arguments into a single string
 		$command = $Args[0]
 		$arguments = $Args[1..$Args.Count]
@@ -30,116 +33,95 @@ function Invoke-Admin () {
 	};
 };
 
-Set-Alias sudo Invoke-Admin
-
 function Invoke-Batstat () {
 	WMIC PATH Win32_Battery Get EstimatedChargeRemaining
 };
 
-$lsd = "$HOME\dev\lsd\target\release\lsd.exe"
-$lsd_debug = "$HOME\dev\lsd\target\debug\lsd.exe"
-
-Set-Alias batstat Invoke-Batstat
-Set-Alias battery Invoke-Batstat
-
 function Invoke-ls() {
 	try {
 		# default to showing all files
-		& $lsd -A $Args
+		& $env:lsd -A $Args
 	}
-	catch {
-		# try to use debug version if there is no release build
-		try {
-			& $lsd_debug -A $Args
-		}
-		catch {
-			Write-Host "Warning: No batch operation, program, or executable matching the pattern of $lsd or $lsd_debug found...`n Please update your powershell profile" -ForegroundColor Yellow
-			Get-ChildItem $Args
-		}
+ catch {
+		Write-Host "Warning: No batch operation, program, or executable matching the pattern of $lsd or $lsd_debug found...`n Please update your powershell profile" -ForegroundColor Yellow
+		Get-ChildItem $Args
 	}
 };
-
-function Invoke-lsd() {
-	try {
-		& $lsd $Args
-	}
-	catch {
-		try {
-			& $lsd_debug $Args
-		}
-		catch {
-			Write-Host "Warning: No batch operation, program, or executable matching the pattern of $lsd or $lsd_debug found...`n Please update your powershell profile" -ForegroundColor Yellow
-			Get-ChildItem $Args
-		}
-	}
-};
-
-Set-Alias lsd Invoke-lsd
-Set-Alias ls Invoke-ls
 
 function Set-Location-Up() {
 	Set-Location ..
 }
 
-Set-Alias .. Set-Location-Up
 
 function Invoke-rm() {
 	Remove-Item $Args -Force -Recurse
 }
 
+# Aliases:
+Set-Alias sudo Invoke-Admin
+Set-Alias batstat Invoke-Batstat
+Set-Alias battery Invoke-Batstat
+Set-Alias ls Invoke-ls
+Set-Alias .. Set-Location-Up
 Set-Alias rm Invoke-rm
 
-# set for starship
-$OS = $env:OS
-Set-Alias $OS $env:OS
-$ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
-Invoke-Expression (& starship init powershell)
+######################################################################
 
-# manages the window title:
-$host.UI.Write("`e]0;$pwd`a")
+function Init() {
+	# set for starship
+	$OS = $env:OS
+	Set-Alias $OS $env:OS
+	$ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
+	Invoke-Expression (& starship init powershell)
 
-Invoke-Expression "$(thefuck --alias)"
+	# manages the window title:
+	$host.UI.Write("`e]0;$pwd`a")
 
-# Config PSReadLine
-Set-PSReadLineOption -EditMode Windows
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-# Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-# `ForwardChar` accepts the entire suggestion text when the cursor is at the end of the line.
-# This custom binding makes `RightArrow` behave similarly - accepting the next word instead of the entire suggestion text.
-Set-PSReadLineKeyHandler -Key Shift+RightArrow `
-	-BriefDescription ForwardCharAndAcceptNextSuggestionWord `
-	-LongDescription "Move cursor one character to the right in the current editing line and accept the next word in suggestion when it's at the end of current editing line" `
-	-ScriptBlock {
-	param($key, $arg)
+	Invoke-Expression "$(thefuck --alias)"
 
-	$line = $null
-	$cursor = $null
-	[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+	# Config PSReadLine
+	Set-PSReadLineOption -EditMode Windows
+	Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+	# Set-PSReadLineOption -PredictionSource History
+	Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+	Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+	# `ForwardChar` accepts the entire suggestion text when the cursor is at the end of the line.
+	# This custom binding makes `RightArrow` behave similarly - accepting the next word instead of the entire suggestion text.
+	Set-PSReadLineKeyHandler -Key Shift+RightArrow `
+		-BriefDescription ForwardCharAndAcceptNextSuggestionWord `
+		-LongDescription "Move cursor one character to the right in the current editing line and accept the next word in suggestion when it's at the end of current editing line" `
+		-ScriptBlock {
+		param($key, $arg)
 
-	if ($cursor -lt $line.Length) {
-		[Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
+		$line = $null
+		$cursor = $null
+		[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+		if ($cursor -lt $line.Length) {
+			[Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
+		}
+		else {
+			[Microsoft.PowerShell.PSConsoleReadLine]::AcceptNextSuggestionWord($key, $arg)
+		}
 	}
-	else {
-		[Microsoft.PowerShell.PSConsoleReadLine]::AcceptNextSuggestionWord($key, $arg)
+
+	Set-PSReadLineKeyHandler -Key Shift+Tab `
+		-BriefDescription ForwardCharAndAcceptNextSuggestionWord `
+		-LongDescription "Move cursor one character to the right in the current editing line and accept the next word in suggestion when it's at the end of current editing line" `
+		-ScriptBlock {
+		param($key, $arg)
+
+		$line = $null
+		$cursor = $null
+		[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+		if ($cursor -lt $line.Length) {
+			[Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
+		}
+		else {
+			[Microsoft.PowerShell.PSConsoleReadLine]::AcceptSuggestion($key, $arg)
+		}
 	}
 }
 
-Set-PSReadLineKeyHandler -Key Shift+Tab `
-	-BriefDescription ForwardCharAndAcceptNextSuggestionWord `
-	-LongDescription "Move cursor one character to the right in the current editing line and accept the next word in suggestion when it's at the end of current editing line" `
-	-ScriptBlock {
-	param($key, $arg)
-
-	$line = $null
-	$cursor = $null
-	[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-
-	if ($cursor -lt $line.Length) {
-		[Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
-	}
-	else {
-		[Microsoft.PowerShell.PSConsoleReadLine]::AcceptSuggestion($key, $arg)
-	}
-}
+Init
